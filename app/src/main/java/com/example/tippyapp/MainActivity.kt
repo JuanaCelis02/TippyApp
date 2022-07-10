698016e58278d6c349253bd7553260377f6dad00
juana.celis@uptc.edu.co <juana.celis@uptc.edu.co>
2022-07-09 20:25:24 -0500

    Creacion de clase anonima para implementar interfaz y obtener el valor de la barra de busqueda

--- app/src/main/java/com/example/tippyapp/MainActivity.kt
+++ app/src/main/java/com/example/tippyapp/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.tippyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
+import com.example.tippyapp.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.seekBarTip.progress = INITIAL_TIP_PERCENT
+        binding.tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
+        binding.seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                //Imprime en LogCat el valor de la barra de progreso
+                Log.i(TAG, "onProgressChanged $progress")
+                binding.tvTipPercentLabel.text = "$progress%"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+
+        
     }
+
 }
